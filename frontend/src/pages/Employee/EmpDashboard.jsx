7634b85517650ff36b5fe75fe611787885a1070f
imhoanq <maiviethoang25052004@gmail.com>
2025-11-26 13:55:18 +0700

    Hoan thanh chuc nang ABC

--- frontend/src/pages/Employee/EmpDashboard.jsx
+++ frontend/src/pages/Employee/EmpDashboard.jsx
@@ -5,7 +5,7 @@ export default function EmpDashboard() {
     return (
         <div className="bg-white border rounded-xl p-6 shadow-sm">
             <h2 className="text-xl font-bold">Tổng quan</h2>
-            <p className="text-gray-600 mt-2">Bạn đang ở khu vực dành cho nhân viên.</p>
+            <p className="text-gray-600 mt-2">Nhân viên.</p>
         </div>
     );
 }
